53b2a3e12fdee5d2e12d4db58ec656edebe3926e
Jax Clark <jamaecla@gmail.com>
2020-01-25 13:03:04 -0700

    tweaking projects

--- src/Projects.js
+++ src/Projects.js
@@ -16,13 +16,13 @@ export default function Projects() {
                 <p>Please note that slow loading time is due to how herokuapp handles images, and not the code itself.</p>
                 <p>Content inside an iframe loses its responsiveness, but the websites themselves are responsive.</p>
             </div>
-            <div className='project1'>
-                <a href="https://plotchaser.herokuapp.com/login">Full-Stack Writer's Helper App</a>
-                <a href="https://plotchaser.herokuapp.com/login"><iframe title='Plot Chaser' src="https://plotchaser.herokuapp.com/login" alt="project1"/></a>
-            </div>
             <div className='project2'>
                 <a href="https://cluemurdermystery.herokuapp.com/">Full-Stack Clue Murder Mystery Game</a>
                 <a href="https://cluemurdermystery.herokuapp.com/"><iframe title='Clue Murder Mystery' src="https://cluemurdermystery.herokuapp.com/" alt="project2"/></a>
+            </div>
+            <div className='project1'>
+                <a href="https://plotchaser.herokuapp.com/login">Full-Stack Writer's Helper App</a>
+                <a href="https://plotchaser.herokuapp.com/login"><iframe title='Plot Chaser' src="https://plotchaser.herokuapp.com/login" alt="project1"/></a>
             </div>
             <div className='project3'>
                 <a href="http://jax-hp-react-app.surge.sh/">Frontend Harry Potter Axios Project</a>
