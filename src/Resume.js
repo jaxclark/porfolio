d1ba95c5cee60ce6f1307719a0be4a79016d47d3
Jax Clark <jamaecla@gmail.com>
2020-03-04 18:29:13 -0700

    update resume

--- src/Resume.js
+++ src/Resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
     return(
         <div className='resume'>
             <div className='downloadResume'>
-                <a download href='https://cdn.discordapp.com/attachments/632280517013930044/670792727864016896/JaxClark_Resume.pdf'>Download Resume</a>
+                <a download href='https://cdn.discordapp.com/attachments/632280517013930044/684935278536032276/JaxClark_Resume.pdf'>Download Resume</a>
             </div>
             <img className='scale-in-center' src="https://cdn.discordapp.com/attachments/632280517013930044/670793037546389514/JaxClark_Resume.png" alt="Resume"/>
         </div>
